c94ff6ecabedb5b1a71f8f9d18523e147c676988
gaganalakruwa <gaganalakruwan.zincat@gmail.com>
2022-06-15 20:07:48 +0530

    add permission

--- src/screens/Restaurants.js
+++ src/screens/Restaurants.js
@@ -147,7 +147,6 @@ const Restaurants = () => {
   return (
     <View style={styles.container}>
 
-<<<<<<< HEAD
       {currentLocation_loaded ?
         <MapView
           style={styles.map}
@@ -178,28 +177,6 @@ const Restaurants = () => {
         : <>
         </>
       }
-=======
-      <MapView
-        style={styles.map}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-        provider={PROVIDER_GOOGLE}
-        showsIndoors={true}
-        zoomEnabled={true}
-        zoomTapEnabled={true}
-        showsScale={true}
-        showsBuildings={true}
-        showsCompass={true}
-        
-      // region={{
-      //   latitude: 37.78825,
-      //   longitude: -122.4324,
-      //   latitudeDelta: 0.015,
-      //   longitudeDelta: 0.0121,
-      // }}
-      >
-      </MapView>
->>>>>>> f36efc9989e04d52e3351783d077c8ff32397b39
 
       {/* <MapView
         // onPress={() => Keyboard.dismiss()}
