3a7b9cc341db7da4e8909a8da74024ada4251e28
gaganalakruwa <gaganalakruwan.zincat@gmail.com>
2022-06-15 19:31:03 +0530

    no

--- src/screens/Weather.js
+++ src/screens/Weather.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { getWeatherData } from "../../service/api/weatherService";
 import BasicStyles from "../styles/BasicStyles";
 
 const Weather = () => {
+
+const[weatherData,setWeatherData]=useState([])
+
+  const getWeatherDetails=()=>{
+
+    getWeatherData().then(
+      response => {
+
+        this.setState({ spinner: false });
+        console.log('reaponse data: ', response);
+
+      }
+    ).catch(error => {
+      if (error.message == "Request failed with status code 404") {
+        showError("Connection error! Please try again.");
+      } else {
+        showError(error.message);
+      }
+    })
+  }
   return (
     <View style={styles.container}>
       <Icon name="ios-rainy" size={30} color={BasicStyles.COLORS.BLUE} />
